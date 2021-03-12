35341ff6b4cb998e3010ce5f1b37f5e97a4c57b1
Elbadj <38667643+Elbadj@users.noreply.github.com>
2021-03-12 12:17:55 +0100

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Question;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()
+            ->count(3)
+            ->has(
+                Question::factory()
+                    ->count(6))
+            ->create();
     }
 }
